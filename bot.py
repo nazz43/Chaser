2c6ef441368cfa56735c2835b6dd8bd3d8fd725e
nazz43 <nazzq53@gmail.com>
2025-07-22 13:55:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1001,12 +1001,12 @@ async def main():
 
 def add_test_data():
     # Додаємо тестові товари
-    liquid_10ml_id = add_product("рідина Chaser", "Фруктовий мікс", "10мл", 160)
+    liquid_10ml_id = add_product("рідина Chaser", "об'єм", "10мл", 160)
     if liquid_10ml_id:
         add_flavors(liquid_10ml_id,
                     "м'ята🍃,кавун ментол🍃🍉,вишня ментол🍒🍃,чорниця ментол🫐🍃,жовтий драгон фрукт💛🐲,жовта черешня🍒💛,Гранат🍅,Виноград🍇,Кавун🍉")
 
-    liquid_30ml_id = add_product("рідина Chaser", "Фруктовий мікс", "30мл", 300)
+    liquid_30ml_id = add_product("рідина Chaser", "об'єм", "30мл", 300)
     if liquid_30ml_id:
         add_flavors(liquid_30ml_id,
                     "Виноград🍇,Полуниця🍓,банан полуниця🍓🍌")
